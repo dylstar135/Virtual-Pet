9079873cc3c447e32a585f1f7b59c92f3b433075
dylstar135 <dylstar160990@googlemail.com>
2024-01-19 10:12:03 +0000

    pet now throws error if pet passes away based on the isAlive getter function

--- src/pet.js
+++ src/pet.js
@@ -1,4 +1,5 @@
 const MAXIMUM_FITNESS = 10;
+const MINIMUM_FITNESS = 0;
 const MINIMUM_HUNGER = 0;
 
 function Pet(name) {
@@ -7,13 +8,27 @@ function Pet(name) {
   this.hunger = MINIMUM_HUNGER;
   this.fitness = MAXIMUM_FITNESS;
 }
+
+Pet.prototype = {
+  get isAlive() {
+    return this.age <= 30 && this.hunger < 10 && this.fitness > 0;
+  },
+};
+
 Pet.prototype.growUp = function () {
   this.age += 1;
-  this.hunger += 5;
-  this.fitness -= 3;
+  this.hunger += 3;
+  if (this.fitness - 3 >= MINIMUM_FITNESS) {
+    this.fitness -= 3;
+  } else {
+    this.fitness = MINIMUM_FITNESS;
+  }
 };
 
 Pet.prototype.walk = function () {
+  if (!this.isAlive) {
+    throw new Error(`Sadly ${this.name} has passed away`);
+  }
   if (this.fitness + 4 <= 10) {
     this.fitness += 4;
   } else {
@@ -22,26 +37,14 @@ Pet.prototype.walk = function () {
 };
 
 Pet.prototype.feed = function () {
+  if (!this.isAlive) {
+    throw new Error(`Sadly ${this.name} has passed away`);
+  }
   if (this.hunger - 3 >= MINIMUM_HUNGER) {
     this.hunger -= 3;
   } else {
     this.hunger = MINIMUM_HUNGER;
   }
-
-  Pet.prototype.checkup = function () {
-    const HUNGRY = "I am hungry";
-    const UNFIT = "I need a walk";
-    const HUNGRY_AND_UNFIT = `${HUNGRY} and ${UNFIT}`;
-    const HAPPY = "I'm happy";
-
-    const isHungry = this.hunger >= 5;
-    const isUnfit = this.fitness <= 3;
-
-    if (isHungry && isUnfit) return HUNGRY_AND_UNFIT;
-    if (isHungry) return HUNGRY;
-    if (isUnfit) return UNFIT;
-    return HAPPY;
-  };
 };
 Pet.prototype.checkup = function () {
   const HUNGRY = "I am hungry";
@@ -51,7 +54,9 @@ Pet.prototype.checkup = function () {
 
   const isHungry = this.hunger >= 5;
   const isUnfit = this.fitness <= 3;
-
+  if (!this.isAlive) {
+    throw new Error(`Sadly ${this.name} has passed away`);
+  }
   if (isHungry && isUnfit) return HUNGRY_AND_UNFIT;
   if (isHungry) return HUNGRY;
   if (isUnfit) return UNFIT;
