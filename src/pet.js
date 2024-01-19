5ac7343b68f68ac59df9be15bd0a85411439516e
dylstar135 <dylstar160990@googlemail.com>
2024-01-19 10:45:14 +0000

    comments added

--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,7 @@
 const MAXIMUM_FITNESS = 10;
 const MINIMUM_FITNESS = 0;
 const MINIMUM_HUNGER = 0;
+//sets maximum and minimum values for certain properties, allowing for easier readability.
 
 class Pet {
   constructor(name) {
@@ -10,16 +11,19 @@ class Pet {
     this.fitness = MAXIMUM_FITNESS;
     this.children = [];
   }
+  // The Pet class, creates a new instance of Pet with the outlined attributes to start.
 
   get isAlive() {
     return this.age <= 30 && this.hunger < 10 && this.fitness > 0;
   }
+  // getter function that returns a boolean based on the returns in the method, and tells user where there pet is alive true or false
 
   growUp() {
     this.age += 1;
     this.hunger += 3;
     this.fitness = Math.max(this.fitness - 3, MINIMUM_FITNESS);
   }
+  // changes the state of Pet
 
   walk() {
     if (!this.isAlive) {
@@ -27,6 +31,7 @@ class Pet {
     }
     this.fitness = Math.min(this.fitness + 4, MAXIMUM_FITNESS);
   }
+  // changes the fitness state of the Pet, but throws an error is isAlive is false (unfortunatly we cant walk dead pets)
 
   feed() {
     if (!this.isAlive) {
@@ -34,6 +39,7 @@ class Pet {
     }
     this.hunger = Math.max(this.hunger - 3, MINIMUM_HUNGER);
   }
+  // changes the hunger state of the Pet, but throws an error is isAlive is false (unfortunatly we cant feed dead pets)
 
   checkup() {
     const HUNGRY = "I am hungry";
@@ -53,11 +59,13 @@ class Pet {
     if (isUnfit) return UNFIT;
     return HAPPY;
   }
+  // Checkup allows the user to check on theur pet, and will return a message instructing them what their pet needs to stay alive
 
   haveChild(babyname) {
     const Child = new Pet(babyname);
     this.children.push(Child);
   }
+  // Your pet can know breed in abundance!
 }
 
 module.exports = Pet;
