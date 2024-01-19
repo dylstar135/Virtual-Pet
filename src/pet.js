ee22f1e1cddef4945e9be977762894f0b9024a25
dylstar135 <dylstar160990@googlemail.com>
2024-01-19 10:16:23 +0000

    refractored code, and used the es6 to replace function with pet class making it easier to read

--- src/pet.js
+++ src/pet.js
@@ -2,65 +2,56 @@ const MAXIMUM_FITNESS = 10;
 const MINIMUM_FITNESS = 0;
 const MINIMUM_HUNGER = 0;
 
-function Pet(name) {
-  this.name = name;
-  this.age = 0;
-  this.hunger = MINIMUM_HUNGER;
-  this.fitness = MAXIMUM_FITNESS;
-}
-
-Pet.prototype = {
-  get isAlive() {
-    return this.age <= 30 && this.hunger < 10 && this.fitness > 0;
-  },
-};
-
-Pet.prototype.growUp = function () {
-  this.age += 1;
-  this.hunger += 3;
-  if (this.fitness - 3 >= MINIMUM_FITNESS) {
-    this.fitness -= 3;
-  } else {
-    this.fitness = MINIMUM_FITNESS;
-  }
-};
-
-Pet.prototype.walk = function () {
-  if (!this.isAlive) {
-    throw new Error(`Sadly ${this.name} has passed away`);
-  }
-  if (this.fitness + 4 <= 10) {
-    this.fitness += 4;
-  } else {
+class Pet {
+  constructor(name) {
+    this.name = name;
+    this.age = 0;
+    this.hunger = MINIMUM_HUNGER;
     this.fitness = MAXIMUM_FITNESS;
   }
-};
 
-Pet.prototype.feed = function () {
-  if (!this.isAlive) {
-    throw new Error(`Sadly ${this.name} has passed away`);
+  get isAlive() {
+    return this.age <= 30 && this.hunger < 10 && this.fitness > 0;
   }
-  if (this.hunger - 3 >= MINIMUM_HUNGER) {
-    this.hunger -= 3;
-  } else {
-    this.hunger = MINIMUM_HUNGER;
-  }
-};
-Pet.prototype.checkup = function () {
-  const HUNGRY = "I am hungry";
-  const UNFIT = "I need a walk";
-  const HUNGRY_AND_UNFIT = `${HUNGRY} and ${UNFIT}`;
-  const HAPPY = "I'm happy";
 
-  const isHungry = this.hunger >= 5;
-  const isUnfit = this.fitness <= 3;
-  if (!this.isAlive) {
-    throw new Error(`Sadly ${this.name} has passed away`);
+  growUp() {
+    this.age += 1;
+    this.hunger += 3;
+    this.fitness = Math.max(this.fitness - 3, MINIMUM_FITNESS);
   }
-  if (isHungry && isUnfit) return HUNGRY_AND_UNFIT;
-  if (isHungry) return HUNGRY;
-  if (isUnfit) return UNFIT;
-  return HAPPY;
-};
+
+  walk() {
+    if (!this.isAlive) {
+      throw new Error(`Sadly ${this.name} has passed away`);
+    }
+    this.fitness = Math.min(this.fitness + 4, MAXIMUM_FITNESS);
+  }
+
+  feed() {
+    if (!this.isAlive) {
+      throw new Error(`Sadly ${this.name} has passed away`);
+    }
+    this.hunger = Math.max(this.hunger - 3, MINIMUM_HUNGER);
+  }
+
+  checkup() {
+    const HUNGRY = "I am hungry";
+    const UNFIT = "I need a walk";
+    const HUNGRY_AND_UNFIT = `${HUNGRY} and ${UNFIT}`;
+    const HAPPY = "I'm happy";
+
+    const isHungry = this.hunger >= 5;
+    const isUnfit = this.fitness <= 3;
+
+    if (!this.isAlive) {
+      throw new Error(`Sadly ${this.name} has passed away`);
+    }
+
+    if (isHungry && isUnfit) return HUNGRY_AND_UNFIT;
+    if (isHungry) return HUNGRY;
+    if (isUnfit) return UNFIT;
+    return HAPPY;
+  }
+}
 
 module.exports = Pet;
